01547e84fbfaf9c9de86b957e02a7c935cdead60
kzmrt <info.murata.kazuhiko@gmail.com>
2019-03-25 16:55:25 +0900

    URLの記述を変更。

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -3,8 +3,8 @@ from rest_framework import routers
 from quickstart import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+router.register('users', views.UserViewSet)
+router.register('groups', views.GroupViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
